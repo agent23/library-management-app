b8c8d248d9afc42d6739c797943fe7ad807f2b8f
L.S Mohale <stephen.mohale@dstvdm.com>
2019-09-04 16:28:28 +0200

    added redis config

--- src/main/java/com/app/library/LibraryApplication.java
+++ src/main/java/com/app/library/LibraryApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Protocol;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 @SpringBootApplication
@@ -15,6 +22,22 @@ public class LibraryApplication {
 
     @RequestMapping("ping")
     public String index(){
+        try {
+            URI redisUri = new URI(System.getenv("REDISCLOUD_URL"));
+            JedisPool pool = new JedisPool(new JedisPoolConfig(),
+                    redisUri.getHost(),
+                    redisUri.getPort(),
+                    Protocol.DEFAULT_TIMEOUT,
+                    redisUri.getUserInfo().split(":",2)[1]);
+            Jedis jedis = pool.getResource();
+            jedis.set("foo", "bar");
+            String value = jedis.get("foo");
+            System.out.println(">>> "+value);
+            // return the instance to the pool when you're done
+            pool.returnResource(jedis);
+        } catch (URISyntaxException e) {
+            // URI couldn't be parsed.
+        }
         return "<h1>Library Application is Alive</h1>";
     }
 
