f881fd5b2ba39a6b6bc4921abf4e174fd7d9d0df
L.S Mohale <stephen.mohale@dstvdm.com>
2019-09-04 14:11:08 +0200

    added a ping endpoint

--- src/main/java/com/app/library/LibraryApplication.java
+++ src/main/java/com/app/library/LibraryApplication.java
@@ -2,12 +2,20 @@ package com.app.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class LibraryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
+    }
+
+    @RequestMapping("ping")
+    public String index(){
+        return "<h1>Library Application is Alive</h1>";
     }
 
 }
