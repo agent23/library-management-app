df63dfc900984e8fa36819a2f65a614918e5c9ba
L.S Mohale <stephen.mohale@dstvdm.com>
2019-09-09 12:04:11 +0200

    added swagger configuration

--- src/main/java/com/app/library/LibraryApplication.java
+++ src/main/java/com/app/library/LibraryApplication.java
@@ -2,43 +2,19 @@ package com.app.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
-import redis.clients.jedis.Protocol;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
-@RestController
 @SpringBootApplication
+@Controller
 public class LibraryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
     }
 
-    @RequestMapping("ping")
-    public String index(){
-        try {
-            URI redisUri = new URI(System.getenv("REDISCLOUD_URL"));
-            JedisPool pool = new JedisPool(new JedisPoolConfig(),
-                    redisUri.getHost(),
-                    redisUri.getPort(),
-                    Protocol.DEFAULT_TIMEOUT,
-                    redisUri.getUserInfo().split(":",2)[1]);
-            Jedis jedis = pool.getResource();
-            jedis.set("foo", "bar");
-            String value = jedis.get("foo");
-            System.out.println(">>> "+value);
-            // return the instance to the pool when you're done
-            pool.returnResource(jedis);
-        } catch (URISyntaxException e) {
-            // URI couldn't be parsed.
-        }
-        return "<h1>Library Application is Alive</h1>";
+    @GetMapping("/")
+    public String redirect(){
+        return "redirect:/swagger-ui.html";
     }
-
 }
--- src/main/java/com/app/library/rest/IndexController.java
+++ src/main/java/com/app/library/rest/IndexController.java
@@ -1,4 +1,14 @@
 package com.app.library.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class IndexController {
+
+    @GetMapping("/ping")
+    public String index(){
+        return "<h2>Library Application is Alive</h2>";
+    }
 }
