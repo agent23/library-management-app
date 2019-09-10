16d79c106b3117ce6e0f79b7f95be4686b0b7c92
L.S Mohale <stephen.mohale@dstvdm.com>
2019-09-10 15:56:56 +0200

    added a password encoder bean

--- src/main/java/com/app/library/LibraryApplication.java
+++ src/main/java/com/app/library/LibraryApplication.java
@@ -2,6 +2,9 @@ package com.app.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -16,5 +19,10 @@ public class LibraryApplication {
     @GetMapping("/")
     public String redirect(){
         return "redirect:/swagger-ui.html";
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
