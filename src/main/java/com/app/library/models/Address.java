5845c813535bc633194075efb9fcd225a30fe5f5
L.S Mohale <stephen.mohale@dstvdm.com>
2019-09-13 10:23:28 +0200

    added no arguments and all arguments constructors annotations

--- src/main/java/com/app/library/models/Address.java
+++ src/main/java/com/app/library/models/Address.java
@@ -1,8 +1,12 @@
 package com.app.library.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
     private String countryISO;
     private int postCode;
--- src/main/java/com/app/library/models/ContactDetails.java
+++ src/main/java/com/app/library/models/ContactDetails.java
@@ -1,8 +1,12 @@
 package com.app.library.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ContactDetails {
     private String areaCode;
     private String phone;
