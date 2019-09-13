890c456f4136829702ee7da933489f7dd776f9fc
L.S Mohale <stephen.mohale@dstvdm.com>
2019-09-13 11:14:15 +0200

    deleted unused files

--- src/main/java/com/app/library/interfaces/IUser.java
+++
@@ -1,8 +0,0 @@
-package com.app.library.interfaces;
-
-public interface IUser {
-    void register(String username, String password, String phone);
-    void update(String username, String password, String phone);
-    void get(String username);
-    void getAll();
-}
