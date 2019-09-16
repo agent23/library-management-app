ad38fad703f56d890f044428c1e934acc5c1b2d6
L.S Mohale <stephen.mohale@dstvdm.com>
2019-09-16 15:47:58 +0200

    added no constructor and all constructor annotations

--- src/main/java/com/app/library/models/User.java
+++ src/main/java/com/app/library/models/User.java
@@ -1,11 +1,15 @@
 package com.app.library.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     private String username;
