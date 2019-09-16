e3f88756b93b2185620fa9e562b56b3650f768cf
L.S Mohale <stephen.mohale@dstvdm.com>
2019-09-16 16:30:07 +0200

    added a method to retrieve all users

--- src/main/java/com/app/library/interfaces/impl/RegistrationServiceImpl.java
+++ src/main/java/com/app/library/interfaces/impl/RegistrationServiceImpl.java
@@ -6,6 +6,8 @@ import com.app.library.services.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class RegistrationServiceImpl implements RegistrationService {
 
@@ -22,6 +24,11 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     public User updateExistingUser(User accountDto) {
         return userRepository.save(setUserDetails(accountDto));
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepository.findAll();
     }
 
     public void deleteUser(String username) {
--- src/main/java/com/app/library/services/RegistrationService.java
+++ src/main/java/com/app/library/services/RegistrationService.java
@@ -2,10 +2,14 @@ package com.app.library.services;
 
 import com.app.library.models.User;
 
+import java.util.List;
+
 public interface RegistrationService {
     User registerNewUserAccount(User accountDto);
 
     void deleteUser(String username);
 
     User updateExistingUser(User accountDto);
+
+    List<User> getUsers();
 }
